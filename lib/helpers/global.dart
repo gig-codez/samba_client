29e7037cd12534651338210239a58e606ccfde3e
Mugamba Bruno <brunohectre@gmail.com>
2024-04-29 13:22:51 +0300

    setup fustal app for production

--- lib/helpers/global.dart
+++ lib/helpers/global.dart
@@ -1,3 +1,3 @@
-String leagueId = "6613ee75c8b9157fe9a2a223"; //"65dc80b91e0e5f4850a681d5";
-String leagueLogo = "assets/leagues/ndejje2.png";
-String appTitle = "Ndejje League App";
+String leagueId = "65a1676750576beff4d9050e"; //"65dc80b91e0e5f4850a681d5";
+String leagueLogo = "assets/leagues/fufa.png";
+String appTitle = "Fustal App";
