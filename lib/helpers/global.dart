5b50d0f016b9d10cecf6f87bf87e22d708696368
Mugamba Bruno <brunohectre@gmail.com>
2024-05-11 13:41:43 +0300

    setup ndejje

--- lib/helpers/global.dart
+++ lib/helpers/global.dart
@@ -1,3 +1,3 @@
-String leagueId = "65dc80b91e0e5f4850a681d5"; //"65dc80b91e0e5f4850a681d5";
-String leagueLogo = "assets/leagues/nangwa.jpeg";
-String appTitle = "Nangwa League App";
+String leagueId = "6613ee75c8b9157fe9a2a223"; //"65dc80b91e0e5f4850a681d5";
+String leagueLogo = "assets/leagues/ndejje2.png";
+String appTitle = "Ndejje League App";
