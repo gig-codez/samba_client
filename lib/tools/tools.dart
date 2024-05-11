439a98dcbc214f62efe2ea006a56cdbdb99c05ad
Mugamba Bruno <brunohectre@gmail.com>
2024-05-11 14:22:33 +0300

    app id added

--- lib/tools/tools.dart
+++ lib/tools/tools.dart
@@ -63,7 +63,7 @@ void showUpdateDialog({String version = '', String latestVersion = ''}) {
                 textColor: Colors.white,
                 onPress: () {
                   StoreRedirect.redirect(
-                    androidAppId: "com.app.ndejje",
+                    androidAppId: appBundle,
                     iOSAppId: "6451200513",
                   ).then((value) => Routes.popPage());
                   // launchPlayStore(context);
