d691a0275d8d194cc861e75526fc6b1da4be12c4
Mugamba Bruno <brunohectre@gmail.com>
2024-05-11 14:04:36 +0300

    modified fau

--- lib/views/pages/news/index.dart
+++ lib/views/pages/news/index.dart
@@ -26,23 +26,6 @@ class _NewsPageState extends State<NewsPage> with TickerProviderStateMixin {
     super.dispose();
   }
 
-class _NewsPageState extends State<NewsPage> with TickerProviderStateMixin {
-  AnimationController? _controller;
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 900),
-      vsync: this,
-    );
-  }
-
-@override
- void dispose() {
-    _controller!.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,17 +62,6 @@ class _NewsPageState extends State<NewsPage> with TickerProviderStateMixin {
                             controller: _controller!,
                             blog: blog,
                           );
-                  },
-                );
-                    ])
-              : ListView.builder(
-                  itemCount: blogs.length,
-                  itemBuilder: (context, index) {
-                    var blog = blogs[index];
-                    return index == 0
-                        ? BlogTrendCard(blog: blog)
-                        :BlogCard(controller:_controller!,blog:blog,)
-                        ;
                   },
                 );
         },
