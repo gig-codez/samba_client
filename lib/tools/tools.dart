def9dca5c78b4659d4b12b9d319be7eeccbc4600
Mugamba Bruno <brunohectre@gmail.com>
2024-05-11 07:38:16 +0300

    modified fau

--- lib/tools/tools.dart
+++ lib/tools/tools.dart
@@ -62,7 +62,7 @@ void showUpdateDialog({String version = '', String latestVersion = ''}) {
                 textColor:Colors.white,
                 onPress: () {
                   StoreRedirect.redirect(
-                    androidAppId: "com.app.ndejje",
+                    androidAppId: "com.app.fau",
                     iOSAppId: "6451200513",
                   ).then((value) => Routes.popPage());
                   // launchPlayStore(context);
--- lib/views/pages/news/index.dart
+++ lib/views/pages/news/index.dart
@@ -14,55 +14,71 @@ class _NewsPageState extends State<NewsPage> {
       body: Consumer<DataController>(
         builder: (context, controller, child) {
           var blogs = controller.blogs;
-          return ListView.builder(
-            itemCount: blogs.length,
-            itemBuilder: (context, index) {
-              var blog = blogs[index];
-              return InkWell(
-                onTap: () {
-                  Routes.animateToPage(BlogDetailPage(blog: blog));
-                },
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+          return blogs.isEmpty
+              ? Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Image.network(
-                      blog.image,
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.width / 2,
-                      fit: BoxFit.cover,
-                    ),
-                    // CachedNetworkImage(
-                    //   imageUrl: blog.image,
-                    //   placeholder: (context, url) => const SpinKitFadingCircle(
-                    //     color: Colors.blue,
-                    //     size: 50.0,
-                    //   ),
-                    //   errorWidget: (context, url, error) =>
-                    //       const Icon(Icons.error),
-                    // ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(18, 5, 18, 0),
-                      child: Text(
-                        blog.title,
-                        style: Theme.of(context).textTheme.bodyLarge!.apply(
-                              fontWeightDelta: 3,
-                              fontSizeDelta: 2,
+                      Center(
+                        child: Text(
+                          "No Blogs yet!!",
+                          style: Theme.of(context).textTheme.titleLarge!.apply(
+                                fontWeightDelta: 3,
+                                fontSizeDelta: 3,
+                              ),
+                        ),
+                      ),
+                    ])
+              : ListView.builder(
+                  itemCount: blogs.length,
+                  itemBuilder: (context, index) {
+                    var blog = blogs[index];
+                    return InkWell(
+                      onTap: () {
+                        Routes.animateToPage(BlogDetailPage(blog: blog));
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network(
+                            blog.image,
+                            width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.width / 2,
+                            fit: BoxFit.cover,
+                          ),
+                          // CachedNetworkImage(
+                          //   imageUrl: blog.image,
+                          //   placeholder: (context, url) => const SpinKitFadingCircle(
+                          //     color: Colors.blue,
+                          //     size: 50.0,
+                          //   ),
+                          //   errorWidget: (context, url, error) =>
+                          //       const Icon(Icons.error),
+                          // ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(18, 5, 18, 0),
+                            child: Text(
+                              blog.title,
+                              style:
+                                  Theme.of(context).textTheme.bodyLarge!.apply(
+                                        fontWeightDelta: 3,
+                                        fontSizeDelta: 2,
+                                      ),
                             ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(18, 5, 18, 5),
+                            child: Text(
+                              blog.content,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 3,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(18, 5, 18, 5),
-                      child: Text(
-                        blog.content,
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 3,
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
-          );
+                    );
+                  },
+                );
         },
       ),
     );
