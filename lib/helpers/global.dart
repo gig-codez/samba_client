8c7a17686f55fbc2d7a0efec9f6189cac8208eec
Mugamba Bruno <brunohectre@gmail.com>
2024-05-16 11:44:43 +0300

    modified global fille

--- lib/helpers/global.dart
+++ lib/helpers/global.dart
@@ -1,4 +1,4 @@
 String leagueId = "65dc80b91e0e5f4850a681d5"; //"65dc80b91e0e5f4850a681d5";
 String leagueLogo = "assets/leagues/nangwa.jpeg";
-String appTitle = "Nangwa League App"; 
+String appTitle = "Nganwa League App"; 
 String appBundle = "com.app.nganwa";
