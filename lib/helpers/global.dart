6dc94ed1783731f99e0f75ac44110901fce242eb
Mugamba Bruno <brunohectre@gmail.com>
2024-05-16 11:30:56 +0300

    setup fau

--- lib/helpers/global.dart
+++ lib/helpers/global.dart
@@ -1,4 +1,4 @@
-String leagueId = "6613ee75c8b9157fe9a2a223"; //"65dc80b91e0e5f4850a681d5";
-String leagueLogo = "assets/leagues/ndejje2.png";
-String appTitle = "Ndejje League App";
-String appBundle = "com.app.ndejje";
+String leagueId = "65a1676750576beff4d9050e";  // ndejje => "6613ee75c8b9157fe9a2a223"; //"65dc80b91e0e5f4850a681d5";
+String leagueLogo = "assets/leagues/fufa.png";
+String appTitle = "Fustal App";
+String appBundle = "com.app.fau";
