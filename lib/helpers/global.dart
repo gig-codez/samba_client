0bb550a6934cdf772f7ae9074c7dd36425fa84d0
Mugamba Bruno <brunohectre@gmail.com>
2024-06-27 09:35:35 +0300

    added required assets for handball app

--- lib/helpers/global.dart
+++ lib/helpers/global.dart
@@ -1,5 +1,9 @@
+/**
+ *  "_id": "667a7d9769692a12fae2c16b",
+            "name": "UGANDA HANDBALL SUPER LEAGUE ",
+ */
 String leagueId =
-    "6613ee75c8b9157fe9a2a223"; // ndejje => "6613ee75c8b9157fe9a2a223"; //"65dc80b91e0e5f4850a681d5";
-String leagueLogo = "assets/leagues/ndejje2.png";
+    "667a7d9769692a12fae2c16b"; // ndejje => "6613ee75c8b9157fe9a2a223"; //"65dc80b91e0e5f4850a681d5";
+String leagueLogo = "assets/leagues/uganda_handball.png";
 String appTitle = "Uganda Handball";
 String appBundle = "com.app.handball";
